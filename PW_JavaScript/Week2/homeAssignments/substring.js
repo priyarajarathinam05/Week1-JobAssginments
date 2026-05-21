f5417ebe084b1c72aa47f6802a402e791b7896ca
priyarajarathinam05 <priya.rajarathinam@gmail.com>
2026-05-21 13:39:54 +0530

    Add files via upload

---
+++ PW_JavaScript/Week2/homeAssignments/substring.js
@@ -0,0 +1,9 @@
+let str="order ID #:123-Confirmed"
+console.log(str.substring(11,14))//substring extract the exact position start and end parameter should be given
+//start index is 11 aned end index14
+
+let stringhh="order"
+console.log(stringhh.substring(7,2))
+console.log(stringhh.slice(7,2))
+
+
