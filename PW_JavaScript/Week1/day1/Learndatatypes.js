5d4052d54bb96c01f279995638f93292085f1da0
priyarajarathinam05 <priya.rajarathinam@gmail.com>
2026-05-21 13:29:34 +0530

    Add files via upload

---
+++ PW_JavaScript/Week1/day1/Learndatatypes.js
@@ -0,0 +1,26 @@
+//number data types -all numbers comes under this datatypes (0,0.1,double)
+console.log(511)
+console.log(511.778)
+
+//sting data type -declaration 
+
+//"double quotes",'single quote',``back tick
+
+console.log(`declaration using tickback`)
+//true or false boolean data types -it can hold any one of the value log
+console.log(true)
+console.log(false)
+
+//var is a keyword for variable 
+var data1=9.455
+console.log(data1)
+
+//undefined data type -no intialization using assignment operator
+var data
+console.log(data)
+//null datatype 
+var data5=null
+
+console.log(data5)
+console.log(typeof data5)
+console.log(typeof data)
